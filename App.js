e463f62900a82456abbbe3eab968a4ba1b099cf7
Deepak <er.d.ahuja@gmail.com>
2019-07-20 22:06:40 +0530

    refactor-step-1 && add CSS

---
+++ App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!")
+  );
+};
+
+ReactDOM.render(App(), document.getElementById("root"));
