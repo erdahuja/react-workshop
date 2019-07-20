3c2cb10a488496702345473c1441fde51441f97b
Deepak <er.d.ahuja@gmail.com>
2019-07-20 23:18:34 +0530

    setup eslint, prettier

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const Pet = (props) => {
+const Pet = props => {
   const { name, animal, breed } = props;
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
@@ -11,8 +11,16 @@ const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, { name: "Tommy", animal: "dog", breed: "Labra" }),
-    React.createElement(Pet, { name: "Jerry", animal: "mouse", breed: "BigBlackRat" }),
-    React.createElement(Pet, { name: "Catty", animal: "cat", breed: "SweetCat" }),
+    React.createElement(Pet, {
+      name: "Jerry",
+      animal: "mouse",
+      breed: "BigBlackRat"
+    }),
+    React.createElement(Pet, {
+      name: "Catty",
+      animal: "cat",
+      breed: "SweetCat"
+    })
   ]);
 };
 
