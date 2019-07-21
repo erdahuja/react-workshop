5837a870d6913d1fd3aee0494fceee479e9e25e3
Deepak <er.d.ahuja@gmail.com>
2019-07-21 08:42:22 +0530

    Introduce JSX

--- App.js
+++ App.js
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Tommy", animal: "dog", breed: "Labra" }),
-    React.createElement(Pet, {
-      name: "Jerry",
-      animal: "mouse",
-      breed: "BigBlackRat"
-    }),
-    React.createElement(Pet, {
-      name: "Catty",
-      animal: "cat",
-      breed: "SweetCat"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Tommy" animal="dog" breed="Labra" />
+      <Pet name="Jerry" animal="mouse" breed="BigBlackRat" />
+      <Pet name="Catty" animal="cat" breed="SweetCat" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
