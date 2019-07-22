b51bede6c0484ba69877ff3be24a27a0c9dfd087
Deepak <er.d.ahuja@gmail.com>
2019-07-22 22:54:03 +0530

    add petfinder api and CDM lifecycle with state

--- App.js
+++ App.js
@@ -1,16 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pf from "petfinder-client";
+
 import Pet from "./Pet";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Tommy" animal="dog" breed="Labra" />
-      <Pet name="Jerry" animal="mouse" breed="BigBlackRat" />
-      <Pet name="Catty" animal="cat" breed="SweetCat" />
-    </div>
-  );
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    }
+  }
+
+  componentDidMount() {
+    petfinder.pet.find({
+    }).then((data) => {
+      let pets;
+      if (data.petfinder.pets && data.petfinder.pets.pet) {
+        if (Array.isArray(data.petfinder.pets.pet)) {
+          pets = data.petfinder.pets.pet;
+        } else {
+          pets = [data.petfinder.pets.pet];
+        }
+      } else {
+        pets = []
+      }
+      this.setState({
+        pets
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        {this.state.pets.map(p => (
+          <Pet
+            key={p.id}
+            media={p.media}
+            location={`${p.contact.city}, ${p.contact.state}`}
+            name={p.name}
+            animal={p.animal}
+            breed={p.breed} />
+        ))
+        }
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
